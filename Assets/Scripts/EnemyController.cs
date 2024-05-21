70d2477d4efaf3bbcdfa6c01c4b364f21e6e9752
MidsoH <masaki.muneyoshi@gmail.com>
2024-05-21 14:06:30 -0400

    5/21

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
@@ -10,8 +11,10 @@ public class EnemyController : MonoBehaviour {
     private Transform playerTransform;
 
 
-    private float moveSpeed = 10;
+    private float moveSpeed = 5;
     private Vector3 moveDirection;
+
+    private bool isAttacking;
 
     // Start is called before the first frame update
     void Start() 
@@ -25,11 +28,44 @@ public class EnemyController : MonoBehaviour {
     void Update() 
     {
         Move();
+        Attack();
     }
 
     private void Move()
     {
-        transform.LookAt(playerTransform);
-        rigidbody.AddForce(transform.forward * moveSpeed, ForceMode.Force);
+        if (!isAttacking) {
+            transform.LookAt(playerTransform);
+            Vector3 direction = new Vector3(playerTransform.position.x - transform.position.x, 0, playerTransform.position.z - transform.position.z);
+                
+
+            rigidbody.AddForce(direction * moveSpeed, ForceMode.Force);
+            Debug.Log(rigidbody.velocity.magnitude);
+
+            Vector3 flatVel = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
+
+            // limit velocity if needed
+            if (flatVel.magnitude > moveSpeed) {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rigidbody.velocity = new Vector3(limitedVel.x, rigidbody.velocity.y, limitedVel.z);
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        Vector3 toPlayer = new Vector3(playerTransform.position.x - transform.position.x, 0, playerTransform.position.z - transform.position.z);
+        if (toPlayer.magnitude < 2) {
+            isAttacking = true;
+        }
+
+        if (isAttacking) {
+            StartCoroutine(wait3sec());
+        }
+    }
+
+    IEnumerator wait3sec()
+    {
+        yield return new WaitForSeconds(2);
+        isAttacking = false;
     }
 }
