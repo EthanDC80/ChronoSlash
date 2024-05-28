7d6be77683d78785f780de6f956265dd99d1ff51
EthanDC80 <89340600+EthanDC80@users.noreply.github.com>
2024-05-28 12:02:24 -0400

    added hitboxes and navmesh for rooms

--- Assets/Scripts/FOVVisualizer.cs
+++ Assets/Scripts/FOVVisualizer.cs
@@ -1,3 +1,4 @@
+/*
 using UnityEditor;
 using UnityEngine;
 
@@ -27,3 +28,4 @@ public class FOVVisualizer : Editor {
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
+*/
